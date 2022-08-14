b344a03d3f83caa9aff7b6298ae82e07a654b3f9
Waldis <91232899+waldis4u@users.noreply.github.com>
2022-08-14 23:17:02 +0100

    Add files via upload

---
+++ new.rb
@@ -0,0 +1,9 @@
+puts "Hello you!"
+puts "Enter your name : "
+name = gets.chomp()
+puts "Enter your age : "
+age = gets.chomp()
+puts ("There once was a man named " + name)
+puts ("he was " + age + " years old...")
+puts ("He really liked the name " + name)
+puts ("but didn't like being " + age)
